64ddee7494357be220490f5abf107cd7a3c8abe9
unknown <sakshambhatnagar308@gmail.com>
2023-12-21 23:17:09 +0530

    crud complete

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,6 +1,9 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goals.model");
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get goals" });
+  const goals = await Goal.find();
+  console.log(goals);
+  res.status(200).json(goals);
 });
 
 const setGoals = asyncHandler(async (req, res) => {
@@ -9,15 +12,32 @@ const setGoals = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add text field");
   } else {
-    res.status(200).json({ message: "post goals" });
+    const goal = await Goal.create({
+      text: req.body.text,
+    });
+    res.status(200).json(goal);
   }
 });
 
 const putGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("goal not found");
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
 });
 
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("goal not found");
+  }
+  const deletedGoal = await Goal.deleteOne({ _id: req.params.id });
+  res.status(200).json({ id: `${req.params.id}` });
 });
 module.exports = { getGoals, setGoals, deleteGoals, putGoals };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
-
+connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
